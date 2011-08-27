7e7a1744a47d18d8029f9e3f26f9baa0989aa959
David Björklund <davidbj@kth.se>
2011-08-27 22:12:37 +0200

    Classes kinda works. Probably some edge cases left, but overall I think that I've one over this.

--- test/awesomeTest.js
+++ test/awesomeTest.js
@@ -1,13 +1,13 @@
 var vows = require('vows'),
     assert = require('assert'),
-    awesome = require('../lib/awesome.js'),
-    date1 = new Date(0),
-    date2 = new Date(500);
+    awesome = require('../lib/awesome.js');
 
-function assertEqual(baseDate, expected){
+function assertEqual(expected){
   var context = {
     topic: function(){
       var selector = this.context.name;
+      var base = this.context.env.context.title.split("'")[1];
+      var baseDate = new Date(base);
       return awesome.next(selector, baseDate);
     }
   };
@@ -18,20 +18,52 @@ function assertEqual(baseDate, expected){
 }
 
 vows.describe('Calculating next occurence').addBatch({
-  "(base 1970-01-01 00:00:00 GMT) with selector":{
-    "year":         assertEqual(date1, date1),
-    "month":        assertEqual(date1, date1),
-    "day":          assertEqual(date1, date1),
-    "hour":         assertEqual(date1, date1),
-    "minute":       assertEqual(date1, date1),
-    "second":       assertEqual(date1, date1)
+  "base: '1970-01-01T00:00:00.0Z' with selector": {
+    "year":         assertEqual(new Date(0)),
+    "year.2011":    assertEqual(new Date("2011-01-01 00:00:00 GMT")),
+    "month":        assertEqual(new Date(0)),
+    "month.6":      assertEqual(new Date("1970-06-01 00:00:00 GMT")),
+    "day":          assertEqual(new Date(0)),
+    "day.27":       assertEqual(new Date("1970-01-27 00:00:00 GMT")),
+    "hour":         assertEqual(new Date(0)),
+    "hour.15":      assertEqual(new Date("1970-01-01 15:00:00 GMT")),
+    "minute":       assertEqual(new Date(0)),
+    "minute.47":    assertEqual(new Date("1970-01-01 00:47:00 GMT")),
+    "second":       assertEqual(new Date(0)),
+    "second.59":    assertEqual(new Date("1970-01-01 00:00:59 GMT")),
+    "minute.4 second.59":
+                    assertEqual(new Date("1970-01-01 00:04:59 GMT")),
+    "hour.15 minute.4 second.59":
+                    assertEqual(new Date("1970-01-01 15:04:59 GMT")),
+    "day.27 hour.15 minute.4 second.59":
+                    assertEqual(new Date("1970-01-27 15:04:59 GMT")),
+    "month.6 day.27 hour.15 minute.4 second.59":
+                    assertEqual(new Date("1970-06-27 15:04:59 GMT")),
+    "year.2011 month.6 day.27 hour.15 minute.4 second.59":
+                    assertEqual(new Date("2011-06-27 15:04:59 GMT")),
   },
-  "(base 1970-01-01 00:00:00 GMT + 500ms) with selector":{
-    "year":         assertEqual(date2, new Date("1971-01-01 00:00:00 GMT")),
-    "month":        assertEqual(date2, new Date("1970-02-01 00:00:00 GMT")),
-    "day":          assertEqual(date2, new Date("1970-01-02 00:00:00 GMT")),
-    "hour":         assertEqual(date2, new Date("1970-01-01 01:00:00 GMT")),
-    "minute":       assertEqual(date2, new Date("1970-01-01 00:01:00 GMT")),
-    "second":       assertEqual(date2, new Date("1970-01-01 00:00:01 GMT"))
-  }
+  "base: '1970-01-01T00:00:00.500Z' with selector": {
+    "year":         assertEqual(new Date("1971-01-01 00:00:00 GMT")),
+    "month":        assertEqual(new Date("1970-02-01 00:00:00 GMT")),
+    "day":          assertEqual(new Date("1970-01-02 00:00:00 GMT")),
+    "hour":         assertEqual(new Date("1970-01-01 01:00:00 GMT")),
+    "minute":       assertEqual(new Date("1970-01-01 00:01:00 GMT")),
+    "second":       assertEqual(new Date("1970-01-01 00:00:01 GMT"))
+  },
+  "base: '1970-01-01T00:00:59.0Z' with selector": {
+    "second.13":    assertEqual(new Date("1970-01-01 00:01:13 GMT"))
+  },
+  "base: '1970-01-01T00:59:00.0Z' with selector": {
+    "minute.13":    assertEqual(new Date("1970-01-01 01:13:00 GMT"))
+  },
+  "base: '1970-01-01T23:00:00.0Z' with selector": {
+    "hour.13":      assertEqual(new Date("1970-01-02 13:00:00 GMT"))
+  },
+  "base: '1970-01-31T00:00:00.0Z' with selector": {
+    "day.13":       assertEqual(new Date("1970-02-13 00:00:00 GMT")),
+    "day.30":       assertEqual(new Date("1970-03-30 00:00:00 GMT"))
+  },
+  "base: '1970-02-29T00:00:00.0Z' with selector": {
+    "day.30":       assertEqual(new Date("1970-03-30 00:00:00 GMT"))
+  },
 }).export(module);
